4a4bb385ff17704b771b7a3c7988390a48e97536
Kevin Rider <github@thebackwaters.com>
2022-06-10 22:19:49 -0600

    Update tests

--- tests/AccountsTest.php
+++ tests/AccountsTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use phpetrade\Accounts;
 use phpetrade\Config;
@@ -13,6 +15,9 @@ final class AccountsTest extends TestCase
         $this->ac_obj = new Accounts($config);
     }
 
+    /**
+     * @return string
+     */
     public function testGetAccountList(): string
     {
         $response = $this->ac_obj->GetAccountList();
@@ -30,7 +35,7 @@ final class AccountsTest extends TestCase
     {
         $account_balance_para["instType"] = "BROKERAGE";
         $account_balance_para["realTimeNAV"] = "true";
-        $ac_balance = $this->ac_obj->GetAccountBalance($account_id_key,$account_balance_para);
+        $ac_balance = $this->ac_obj->GetAccountBalance($account_id_key, $account_balance_para);
         $this->assertIsObject($ac_balance);
         $this->assertTrue(isset($ac_balance->accountId));
         $this->assertTrue(isset($ac_balance->accountType));
@@ -44,7 +49,7 @@ final class AccountsTest extends TestCase
         $account_tran_para["Accept"] = "application/xml";
         $account_tran_para["sortOrder"] = "desc";
         $account_tran_para["count"] = "25";
-        $ac_tran = $this->ac_obj->GetAccountTransactions($account_id_key,$account_tran_para);
+        $ac_tran = $this->ac_obj->GetAccountTransactions($account_id_key, $account_tran_para);
         $this->assertIsObject($ac_tran);
         $this->assertGreaterThan(0, count($ac_tran->Transaction));
         $transaction_id = (int) $ac_tran->Transaction[0]->transactionId;
@@ -55,11 +60,11 @@ final class AccountsTest extends TestCase
     * @depends testGetAccountList
     * @depends testGetAccountTransactions
     */
-    public function testGetAccountTransactionDetails($account_id_key,$transaction_id): void
+    public function testGetAccountTransactionDetails($account_id_key, $transaction_id): void
     {
         $account_tran_id_para["Accept"] = "application/xml";
         $account_tran_id_para["storeId"] = "0";
-        $ac_tran_id = $this->ac_obj->GetAccountTransactionDetails($account_id_key,$transaction_id,$account_tran_id_para);
+        $ac_tran_id = $this->ac_obj->GetAccountTransactionDetails($account_id_key, $transaction_id, $account_tran_id_para);
         $this->assertIsObject($ac_tran_id);
         $this->assertTrue(isset($ac_tran_id->transactionId));
         $this->assertTrue(isset($ac_tran_id->transactionDate));
@@ -77,8 +82,8 @@ final class AccountsTest extends TestCase
         $ac_obj_mock = $this->createMock(Accounts::class);
         $ac_obj_mock->expects($this->once())
              ->method('GetAccountPortfolio')
-             ->will($this->returnValue(true));        
-        $ac_port = $ac_obj_mock->GetAccountPortfolio($account_id_key,$account_port_para);
+             ->will($this->returnValue(true));
+        $ac_port = $ac_obj_mock->GetAccountPortfolio($account_id_key, $account_port_para);
         $this->assertTrue($ac_port);
 
         //No mocking
@@ -92,9 +97,12 @@ final class AccountsTest extends TestCase
     */
     public function testbuildFullUrl($a, $b, $expected): void
     {
-        $this->assertSame($expected, $this->ac_obj->buildFullUrl($a,$b));
+        $this->assertSame($expected, $this->ac_obj->buildFullUrl($a, $b));
     }
 
+    /**
+     * @return array[]
+     */
     public function urlProvider(): array
     {
         return [
--- tests/AuthorizationTest.php
+++ tests/AuthorizationTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use phpetrade\Authorization;
 use phpetrade\Config;
@@ -7,26 +9,38 @@ final class AuthorizationTest extends TestCase
 {
     private $auth_obj;
 
+    /**
+     * @return void
+     */
     protected function setUp(): void
     {
         $config = new Config(true);
         $this->auth_obj = new Authorization($config);
     }
 
+    /**
+     * @return void
+     */
     public function testRenewAccessToken(): void
     {
         $this->assertEquals('Access Token has been renewed', $this->auth_obj->RenewAccessToken());
     }
 
+    /**
+     * @return void
+     */
     public function testRevokeAccessToken(): void
     {
         //There is a space after Token
         $this->assertEquals('Revoked Access Token ', $this->auth_obj->RevokeAccessToken());
     }
 
+    /**
+     * @return void
+     */
     public function test401OnRevoked(): void
     {
-        $this->expectErrorMessageMatches('/401/');
+        $this->expectOutputRegex('/401/');
         $this->auth_obj->RenewAccessToken();
     }
 }
