21ba84696c95efcdbff8298bfdc7bac794e9ce1f
Kevin Rider <github@thebackwaters.com>
2022-06-26 20:28:52 -0600

    lint auth.php

--- auth.php
+++ auth.php
@@ -1,23 +1,23 @@
 <?php
+
 namespace phpetrade;
+
+use OAuthException;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $config = new Config();
 //Setup the OAuth 1.0a exchange.
 //Must have pecl oauth installed and enabled in php.ini.
-try 
-{
-    $oauth = new \OAuth($config->app_key,$config->app_secret);
+try {
+    $oauth = new \OAuth($config->app_key, $config->app_secret);
     //The "oob" call back is required by ETrade API when authenticating.
-    $request_token_info = $oauth->getRequestToken($config->request_token_url,"oob");
-    if(empty($request_token_info)) 
-    {
+    $request_token_info = $oauth->getRequestToken($config->request_token_url, "oob");
+    if (empty($request_token_info)) {
         print "Failed fetching request token, response was: " . $oauth->getLastResponse();
     }
-} 
-catch(OAuthException $E) 
-{
-    echo "Response: ". $E->lastResponse . "\n";
+} catch (OAuthException $E) {
+    echo "Response: " . $E->lastResponse . "\n";
 }
 
 $auth_url =  $config->authorize_url . "?key=" . $config->app_key . "&token=" . urlencode($request_token_info['oauth_token']);
@@ -29,32 +29,26 @@ echo "\n---------------------------------------------------------------\n";
 echo "Please follow the above URL and get the verifier code (required to get the final access token).\n\n";
 
 echo "Please enter the verifier code :";
-$h = fopen ("php://stdin","r");
+$h = fopen("php://stdin", "r");
 $v = fgets($h);
 $v = trim($v);
 fclose($h);
 
-$oauth->setToken($request_token_info['oauth_token'],$request_token_info['oauth_token_secret']);
-try
-{
-    $access_token_info = $oauth->getAccessToken($config->access_token_url,'',$v,'GET');
-}
-catch (OAuthException $E) 
-{
-    echo "Response: ". $E->lastResponse . "\n";
+$oauth->setToken($request_token_info['oauth_token'], $request_token_info['oauth_token_secret']);
+try {
+    $access_token_info = $oauth->getAccessToken($config->access_token_url, '', $v, 'GET');
+} catch (OAuthException $E) {
+    echo "Response: " . $E->lastResponse . "\n";
 }
 
 
-if(isset($access_token_info['oauth_token']) && isset($access_token_info['oauth_token_secret']))
-{
+if (isset($access_token_info['oauth_token']) && isset($access_token_info['oauth_token_secret'])) {
     echo "Final access tokens have been written to tokens.inc";
     echo "\n---------------------------------------------------------------\n";
-    echo "\nToken   : ". $access_token_info['oauth_token'];
-    echo "\nSecret  : ". $access_token_info['oauth_token_secret'];
+    echo "\nToken   : " . $access_token_info['oauth_token'];
+    echo "\nSecret  : " . $access_token_info['oauth_token_secret'];
     echo "\n---------------------------------------------------------------\n";
-}
-else
-{
+} else {
     echo "Could not get request token...\n";
     exit;
 }
